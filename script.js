6a48d55c07b1068821b55fe3d6990abff7ee295e
alineantunesrocha-boop <aline.antunes.rocha@escola.pr.gov.br>
2026-05-13 09:54:32 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-let cartCount = 0;
+let totalItens = 0;
 
-const cartElement = document.getElementById('cart');
-const buttons = document.querySelectorAll('.add-to-cart');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const product = button.parentElement;
-        const productName = product.dataset.name;
-        const productPrice = product.dataset.price;
-
-        cartCount++;
-        cartElement.textContent = `Carrinho: ${cartCount}`;
-
-        alert(`${productName} adicionado ao carrinho por R$ ${productPrice}`);
-    });
-});
+function adicionarAoCarrinho(nome, preco) {
+    totalItens++;
+    document.getElementById('total-itens').textContent = totalItens;
+    alert(`${nome} adicionado ao carrinho por R$ ${preco.toFixed(2)}!`);
+}
